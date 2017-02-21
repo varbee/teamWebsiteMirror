a4b86425b5a1a27e2666f6c9b1d33219112bd761
rounddream <pmy000@foxmail.com>
2017-02-21 15:56:37 +0800

    更改照片效果

--- components/members.js
+++ components/members.js
@@ -4,3 +4,33 @@ import "../assets/css/members.scss";
 import tpl from "../assets/tpl/members.tpl";
 
 $('.members').append(tpl());
+
+$(function () {
+	function scrollFunc(e) {
+        e = e || window.event;
+        if (e.wheelDelta || e.detail) {  //判断浏览器IE，谷歌滑轮事件
+			if ($('.members').hasClass('swiper-slide-active')) {
+				$('.picture').addClass('show-picture');
+			}
+			else {
+				$('.picture').removeClass('show-picture');
+			}
+        } 
+    }
+    //给页面绑定滑轮滚动事件
+    if (document.addEventListener) {
+        document.addEventListener('DOMMouseScroll', scrollFunc, false);
+    }
+    //滚动滑轮触发scrollFunc方法
+    window.onmousewheel = document.onmousewheel = scrollFunc; 
+
+    $('.swiper-pagination-bullet').click(function () {
+    	if ($('.members').hasClass('swiper-slide-active')) {
+				$('.picture').removeClass('show-picture');
+			}
+			else {
+				$('.picture').addClass('show-picture');
+			}
+    });
+
+});
